17b55900fe3cc3f367e0696a231f1e132707baee
Dan O'Sullivan <dan@Dans-MacBook-Air.local>
2023-11-17 20:18:59 +0000

    first commit

---
+++ jpegs.py
@@ -0,0 +1,43 @@
+import os
+import subprocess
+
+def convert_ivf_to_jpegs(input_ivf, output_pattern):
+    ffmpeg_command = [
+        "ffmpeg",
+        "-i", input_ivf,
+        "-vf", "fps=1",  # Adjust frame rate if needed
+        output_pattern
+    ]
+
+    try:
+        subprocess.run(ffmpeg_command, check=True)
+        print("IVF file converted to JPEGs successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error converting IVF to JPEGs: {e}")
+
+def find_jpeg_frames(directory):
+    jpeg_frames = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".jpg") or file.endswith(".jpeg"):
+                jpeg_frames.append(os.path.join(root, file))
+    return jpeg_frames
+
+directory_path = "/Users/dan/Desktop/softwareDev/save-to-disk"
+input_ivf = "/Users/dan/Desktop/softwareDev/save-to-disk/output.ivf"
+output_pattern = "/Users/dan/Desktop/softwareDev/save-to-disk/output%04d.jpg"
+
+# Convert IVF to JPEGs
+convert_ivf_to_jpegs(input_ivf, output_pattern)
+
+# Find JPEG keyframes in the directory
+jpeg_files = find_jpeg_frames(directory_path)
+
+if jpeg_files:
+    print("Success! JPEG files found:")
+    for jpeg_file in jpeg_files:
+        print(jpeg_file)
+    # Add your code to interact with the AI processing application here
+else:
+    print("No JPEG files found.")
+
